986bd16af27520a319622a6d596c4908c13f226a
OctoRocket <xteamxeno@gmail.com>
2024-12-06 18:12:44 -0600

    Add basic CORS support

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ mod github;
 use axum::{handler::Handler, routing::get, Router, http::StatusCode};
 use clap::Parser;
 use github::AstaCommitApi;
+use http::Method;
+use tower::ServiceBuilder;
+use tower_http::cors::{self, CorsLayer};
 use std::sync::Arc;
 use tokio::{net::TcpListener, sync::Mutex};
 
@@ -13,7 +16,16 @@ async fn main() {
     let args = args::Args::parse();
     let commit_api = AstaCommitApi::new();
 
-    let route = Router::new().route("/", get(get_commits(Arc::new(Mutex::new(commit_api)))));
+    let cors = CorsLayer::new()    // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET, Method::POST])
+        // allow requests from any origin
+        .allow_origin(cors::Any);
+
+    let middleware = ServiceBuilder::new().layer(cors);
+    let route = Router::new()
+        .route("/", get(get_commits(Arc::new(Mutex::new(commit_api)))))
+        .layer(middleware);
+
     let listener = match TcpListener::bind(format!("127.0.0.1:{}", args.port)).await {
         Ok(v) => v,
         Err(_) => {
